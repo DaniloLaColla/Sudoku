71a7ddc4ff474ff2a167afb7027dbe4c155ce2ca
Danilo La Colla <dalaco618@gmail.com>
2020-10-25 17:43:56 -0300

    se agrega JAR y comentarios en Cronometro

--- GUI/Cronometro.java
+++ GUI/Cronometro.java
@@ -14,20 +14,15 @@ import javax.swing.JPanel;
 public class Cronometro extends JPanel implements Runnable, ActionListener{
 public static int onoff = 0;
 
-// Ajuste al codigo: estas variables se pasan al comienzo de la clase.
 JLabel tiempo;
 Thread hilo;
 boolean cronometroActivo;
 boolean pausar;
 
-// Ajuste al codigo: se crea variable iniciado de tipo booleano se inicializa en verdadero.
 boolean iniciado = true;
-//*****************************************************************************************
 
-public Cronometro()    {
-	//setTitle("Cronometro");
+public Cronometro()    {  //constructor cronometro
 	setSize( 500, 200 );
-	//setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	setLayout( new BorderLayout() );
 
 	//Etiqueta donde se colocara el tiempo 
@@ -51,11 +46,11 @@ public Cronometro()    {
 	add( btnP, BorderLayout.EAST );
 
 	JButton btnD = new JButton( "Detener" );
-	// btnD.setVisible(false);
+	
 	btnD.addActionListener( this );
 	add( btnD, BorderLayout.SOUTH );
 
-	//this.setLocationRelativeTo( null );
+	
 	setVisible( true );
 }
 
@@ -65,22 +60,19 @@ public void run(){
 	String min="", seg="", mil="";
 	try {
 
-		//Mientras cronometroActivo sea verdadero entonces seguira
-		//aumentando el tiempo
+		//Mientras cronometroActivo sea verdadero entonces seguira aumentando el tiempo
 		while( cronometroActivo ) {
-			// Ajuste al codigo: se elimina while y se anexa un if donde se niega la variable pausar	
+				
 			if (!pausar) {
-				//**************************************************************************************
+				
 				Thread.sleep( 4 );
 				//Incrementamos 4 milesimas de segundo
 				milesimas += 4;
-				//Cuando llega a 1000 osea 1 segundo aumenta 1 segundo
-				//y las milesimas de segundo de nuevo a 0
+				//Cuando llega a 1000 osea 1 segundo aumenta 1 segundo y las milesimas de segundo de nuevo a 0
 				if( milesimas == 1000 ) {
 					milesimas = 0;
 					segundos += 1;
-					//Si los segundos llegan a 60 entonces aumenta 1 los minutos
-					//y los segundos vuelven a 0
+					//Si los segundos llegan a 60 entonces aumenta 1 los minutos y los segundos vuelven a 0
 					if( segundos == 60 )
 					{
 						segundos = 0;
@@ -88,8 +80,7 @@ public void run(){
 					}
 				}
 
-				//Esto solamente es estetica para que siempre este en formato
-				//00:00:000
+				//Esto solamente es estetica para que siempre este en formato 00:00:000
 				if( minutos < 10 ) min = "0" + minutos;
 				else min = minutos.toString();
 				if( segundos < 10 ) seg = "0" + segundos;
@@ -110,7 +101,7 @@ public void run(){
 	tiempo.setText( "00:00:000" );
 }
 
-//Esto es para el boton iniciar y reiniciar
+//Oyentes de botones
 public void actionPerformed( ActionEvent evt ) {
 	Object o = evt.getSource();
 	if( o instanceof JButton ) {
@@ -127,10 +118,9 @@ public void actionPerformed( ActionEvent evt ) {
 	}
 }
 
-//Iniciar el cronometro poniendo cronometroActivo 
-//en verdadero para que entre en el while
+//Iniciar el cronometro poniendo cronometroActivo en verdadero para que entre en el while
 public void iniciarCronometro() {
-	// Ajuste al código: se crea if esto para que cuando se vuelva oprimir iniciar no llame a otro hilo e impida que se vea como si estuviera en error.
+	//este if es para que cuando se vuelva oprimir iniciar no llame a otro hilo e impida que se vea como si estuviera en error.
 	if (iniciado) {
 		hilo = new Thread( this ); 	
 		cronometroActivo = true;
@@ -141,28 +131,22 @@ public void iniciarCronometro() {
 	}        
 }
 
-//Esto es para detener el cronometro
+//Detener el cronometro
 public void pararCronometro(){
 	pausar = true ;
-	// Ajuste al codigo: cada vez que el cronometro sea detenido se habilita varaible iniciado para que se pueda crear una nueva instancia de hilo
+	
 	iniciado = true;
-	//********************************************************************************************************************************************
 }
 
 public void reiniciarCronometro(){
 	cronometroActivo = false;
 	iniciado = true;
-	// Ajuste al codigo: se adiciona esta linea para que deje el label en formato "00:00:00" 
 	tiempo.setText( "00:00:000" );
-	//****************************************************************************************
-}
+	}
 
 public String getTime() {
 	pararCronometro();
 	return tiempo.getText();
 }
-/*
-public static void main(String[] args) {		
-	new Cronometro();
-}*/
+
 }
